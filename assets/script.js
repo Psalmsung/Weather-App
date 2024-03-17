f494e1a16a98491a5405e20e1665a34799d4ea62
Sunday Abuoma <sundayabuoma@gmail.com>
2024-03-17 02:41:49 +0100

    Added the api in the script file

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+const apiKey = "";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=abuja";
+async function checkWeather(){
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    var data = await response.json();
+    console.log(data);
+}
